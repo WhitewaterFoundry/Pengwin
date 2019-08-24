118122a2d33eda491b4ed9ac8577a3f3adb0c054
sirredbeard <hayden@whitewaterfoundry.com>
2019-08-24 12:48:22 -0400

    Azure Pipelines

--- build.bat
+++ build.bat
@@ -44,35 +44,10 @@ if %MSBUILD%==() (
     goto :EXIT
 ) 
 :FOUND_MSBUILD
-set _MSBUILD_TARGET=Build
-set _MSBUILD_CONFIG=Debug
-set _MSBUILD_PLATFORM=x64
-set _MSBUILD_APPX_BUNDLE_PLATFORMS="x64|ARM64"
 
-:ARGS_LOOP
-if (%1) == () goto :POST_ARGS_LOOP
-if (%1) == (clean) (
-    set _MSBUILD_TARGET=Clean,Build
-)
-if (%1) == (rel) (
-    set _MSBUILD_CONFIG=Release
-)
-if (%1) == (x64) (
-    set _MSBUILD_PLATFORM=x64
-    set _MSBUILD_APPX_BUNDLE_PLATFORMS=x64
-)
-if (%1) == (ARM64) (
-    set _MSBUILD_PLATFORM=ARM64
-    set _MSBUILD_APPX_BUNDLE_PLATFORMS=ARM64
-)
-shift
-goto :ARGS_LOOP
-
-:POST_ARGS_LOOP
 %MSBUILD% %~dp0\DistroLauncher.sln /t:%_MSBUILD_TARGET% /m /nr:true ^
-    /p:Configuration=%_MSBUILD_CONFIG% ^
-    /p:Platform=%_MSBUILD_PLATFORM% ^
-    /p:AppxBundlePlatforms=%_MSBUILD_APPX_BUNDLE_PLATFORMS% ^
+    /p:Configuration=Release ^
+    /p:Platform="x64" ^
     /p:UseSubFolderForOutputDirDuringMultiPlatformBuild=false
 
 if (%ERRORLEVEL%) == (0) (
