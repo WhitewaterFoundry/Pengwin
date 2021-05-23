cdc0323e6149559948fee0e99ad3c0812daeb208
Carlos Ramirez <crramirez@gmail.com>
2021-05-23 16:44:39 +0200

    * Enables put notifications so you will receive a message everytime a new Pengwin version is released

--- DistroLauncher/DistroLauncher.cpp
+++ DistroLauncher/DistroLauncher.cpp
@@ -14,11 +14,11 @@
 #define ARG_RUN_C               L"-c"
 
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.System.h>
 #include <winrt/Windows.Storage.h>
 
 
 using namespace winrt;
-using namespace Windows::UI::ViewManagement;
 using namespace Windows::Foundation;
 using namespace Windows::System;
 using namespace Windows::Storage;
@@ -136,6 +136,26 @@ fire_and_forget SyncIcon(const hstring& iconName)
     }
 }
 
+
+// ReSharper disable once IdentifierTypo
+fire_and_forget ShowPengwinUi()
+{
+    // ReSharper disable once CppTooWideScope
+    const auto file =
+        co_await ApplicationData::Current().LocalFolder().TryGetItemAsync(L"MicrosoftStoreEngagementSDKId.txt");
+
+    if (! file)
+    {
+        // ReSharper disable once StringLiteralTypo
+        const hstring str = L"pengwinui://";
+
+        const auto uri = Uri(str);
+
+        co_await Launcher::LaunchUriAsync(uri);
+    }
+}
+
+// ReSharper disable once IdentifierTypo
 int wmain(int argc, const wchar_t* argv[])
 {
     // Update the title bar of the console window.
@@ -187,9 +207,11 @@ int wmain(int argc, const wchar_t* argv[])
     // Parse the command line arguments.
     if ((SUCCEEDED(hr)) && (!installOnly))
     {
+        // ReSharper disable once StringLiteralTypo
         SyncIcon(L"pengwin");
         SyncIcon(L"background1");
         SyncIcon(L"background2");
+        ShowPengwinUi();
 
         if (arguments.empty())
         {
@@ -202,8 +224,8 @@ int wmain(int argc, const wchar_t* argv[])
                 Helpers::PromptForInput();
             }
         }
-        else if ((arguments[0] == ARG_RUN) ||
-            (arguments[0] == ARG_RUN_C))
+        else if (arguments[0] == ARG_RUN ||
+            arguments[0] == ARG_RUN_C)
         {
             std::wstring command;
             for (size_t index = 1; index < arguments.size(); index += 1)
